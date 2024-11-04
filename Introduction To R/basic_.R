eabbc13b449907f14adec787799a7467516dcab6
AFM Rafiul Hassan <114606137+afmrafiulhassan@users.noreply.github.com>
2024-11-04 23:44:46 +0600

    Add files via upload

---
+++ Introduction To R/basic_.R
@@ -0,0 +1,79 @@
+1+1
+2+3
+3+4
+
+
+
+3+5*2
+5^5
+sqrt(5)
+pi
+
+
+
+x=2
+y<-5
+z<-7
+x*y*z
+
+
+
+# X*y*z
+# x=2
+# y<-5
+# z=7
+# x*y*Z
+
+
+x=2.5
+y<-5
+z<-7
+
+
+abs(x)
+sqrt(x)
+ceiling(x)
+floor(x)
+
+
+s="Hello World"
+tolower(s)
+toupper(s)
+strsplit(s,split = "")
+strsplit(s,split = " ")
+strsplit(s,split = "o")
+
+
+nchar(s)
+
+
+a<- "Hello"
+b<- "World"
+paste(a,b)
+
+
+
+paste0(a,b)
+#or
+paste(a,b,sep="")
+
+
+
+x="What's
+That!"
+x
+cat(x)
+
+
+
+
+a<-50
+b<-20
+
+if(a>b) 
+{
+  print("a is greater than b")
+}else
+{
+  print("a is not greater than b")
+}
